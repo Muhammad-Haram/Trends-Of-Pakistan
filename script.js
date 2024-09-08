71d5376eecfa99d4a46153616002d6683aa1ce0b
Muhammad Haram <haramh643@gmail.com>
2024-09-09 00:35:13 +0500

    add some cards and swipeer

--- script.js
+++ script.js
@@ -47,5 +47,24 @@ body.addEventListener("mousemove", function (location) {
 
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
-    spaceBetween: 30,
+    spaceBetween: 20,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    
   });
